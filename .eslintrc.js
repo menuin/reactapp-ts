6f72f307b48cd3a0435601f61a4e5a01cec8cd2c
menuin <menuin@hanyang.ac.kr>
2021-09-25 14:49:14 +0900

    create react app

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/error",
+    "plugin:import/warning",
+    "airbnb",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+  },
+};
